4f58b97b5297a342acbd1bf188ded4490f8604e7
Unknown <alfredo.silva.13@sansano.usm.cl>
2017-04-29 22:55:49 -0300

    Trabajo con google empezado
    se definio la libreria google 1.9.3

--- Proyecto_K-Search/K_Search/buscador/views.py
+++ Proyecto_K-Search/K_Search/buscador/views.py
@@ -2,11 +2,78 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from aplicacion.models import *
+from django.contrib.auth.models import User #// Sistema de usuarios proporcionado por Django
+from django.shortcuts import get_object_or_404, render, redirect #// Tribial de django
+from django.http import HttpResponseRedirect, HttpResponse #// En caso de no querer hacer render
+from django.core.urlresolvers import reverse
+from datetime import datetime, date #// Para manejo de fechas
+from django.contrib.auth import authenticate, login, logout #// Para autentificar los usuarios
+from django.contrib.auth.decorators import login_required #// Para poner antes de las views y requerir login
+from google import search,get_page
+
+
 
 def main_view(request):
-
-	var1 = 5
+	if request.method == "POST":
+		# Ejemplos sacando valores del formulario (de cada input)
+		busqueda = request.POST.get("search")
+		lista_urls = search(busqueda, stop=10)
+    		
 
 	context = {'var1' : var1}
 
 	return render(request, 'buscador/index.html',context)
+
+'''@login_required(login_url="/")
+def add_to_calendar(request, sel_g=None):
+
+	# Variables varias
+	post = 0
+	conected = 0
+	can_inscribe_p = 0
+	can_inscribe_g = 0
+	valid_date = 0
+
+	# Autentificacion de permisos
+
+	if request.user.is_authenticated():
+		conected = 1
+
+		u = request.user
+		up = UserProfile.objects.get(user = u)
+		u_name = up.user_name
+		country = up.country
+
+		if u.has_perm("calendario.can_inscribe_g"):
+			can_inscribe_g = 1
+
+		if u.has_perm("calendario.can_inscribe_p"):
+			can_inscribe_p = 1
+
+		if request.GET.get('btn'):
+			return logout_view(request)
+
+
+	if not can_inscribe_g and u_name != "ADMIN":
+		get_object_or_404(Calendar, pk=-1)
+
+
+	# Si se utiliza el metodo POST en esta ruta
+
+	if request.method == "POST":
+
+		# Ejemplos sacando valores del formulario (de cada input)
+		bol = request.POST.get("bol")
+		sel_grade = request.POST.get("sel_cal")
+		cal_name = request.POST.get("cal_name")
+		programation_status = request.POST.get("programation_status")
+		ejecution_status = request.POST.get("ejecution_status")
+
+	# Hacer cosas con los valores Y...
+
+	# Retornar resultados en el context
+	context = {'grade_list' : grade_list, 'provider_list' : provider_list, 'ceco_list' : ceco_list,'conected' : conected, 'u_name' : u_name, 'country' : country, 'post' : post, 'ceco_s' : ceco_s,'c' : c, 'men' : men, 'sel_g' : sel_g}
+	
+	return render(request, 'calendario/add_to_calendar.html',context)
+'''
